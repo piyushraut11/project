0cbc28481e838e596c2cf46dfe6988c36e3ba751
piyushraut11 <piyushraut795@gmail.com>
2024-12-21 18:22:40 +0530

    Update test_src.py

--- test_src.py
+++ test_src.py
@@ -1,1 +1,13 @@
 # Your tests here
+def long_sub_str(keyword):
+    l_seq= ''
+    for i in range(len(keyword)):
+        for j in range(i+1, len(keyword)):
+            common= "".join(c for c in keyword[i] if c in keyword[j])
+            if len(common) > len(l_seq):
+                l_seq=common
+    return l_seq
+
+keyword = ['milk', 'catalog', 'c++', 'python','cat', 'dog']
+result = long_sub_str(keyword)
+print(result)
