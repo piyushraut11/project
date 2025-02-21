5467dcc3128e0faabd2425f23b865fc2986f3bc9
piyushraut220 <piyushraut220@gmail.com>
2025-02-21 16:49:23 +0530

    initial commit

--- script_1.py
+++ script_1.py
@@ -1,36 +1,7 @@
-def add(x, y):
-    return x + y
+import openpyxl
 
-def subtract(x, y):
-    return x - y
+wb = openpyxl.load_workbook(r"C:\Users\dhana\Downloads\Excel_exercise.xlsx")
 
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
-        return "Error! Division by zero."
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-choice = input("Enter choice (1/2/3/4): ")
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if choice == '1':
-    print(f"{num1} + {num2} = {add(num1, num2)}")
-elif choice == '2':
-    print(f"{num1} - {num2} = {subtract(num1, num2)}")
-elif choice == '3':
-    print(f"{num1} * {num2} = {multiply(num1, num2)}")
-elif choice == '4':
-    print(f"{num1} / {num2} = {divide(num1, num2)}")
-else:
-    print("Invalid input")
+sheet1 = wb['Sheet1']
+print(sheet1)
+#sheet2 = wb['']
